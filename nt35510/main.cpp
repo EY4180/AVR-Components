d00997062eb1bdadab55f62f28148eacc9c788ab
Edward Yamamoto <kfcs700@gmail.com>
2021-07-27 16:15:49 +0800

    Added Entry For mt35510 display driver

---
+++ nt35510/main.cpp
@@ -0,0 +1,17 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/twi.h>
+#include <util/delay.h>
+
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define GPIO_EXPANDER_ADDR 0x20
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
